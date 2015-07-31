7c41054eb84a32740ea12aa036e040bb7bc72041
Raffaello <raffaello.bonghi@officinerobotiche.it>
2015-07-31 15:06:54 +0200

    Added gpio frames in packet list

--- includes/packet/frame_gpio.h
+++ includes/packet/frame_gpio.h
@@ -1,8 +1,18 @@
-/* 
- * File:   frame_gpio.h
- * Author: Raffaello
+/*
+ * Copyright (C) 2015 Officine Robotiche
+ * Author: Raffaello Bonghi
+ * email:  raffaello.bonghi@officinerobotiche.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU Lesser General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
  *
- * Created on July 31, 2015, 2:11 PM
+ * A copy of the license can be found at
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
  */
 
 #ifndef FRAME_GPIO_H
@@ -26,21 +36,27 @@ typedef struct _gpio_set {
     uint8_t number;
     int8_t type;
 } gpio_set_t;
-#define LNG_GPIO_SET sizeof(gpio_set_t)    
+#define LNG_GPIO_SET sizeof(gpio_set_t)   
+
+typedef struct _gpio_analog {
+    
+} gpio_analog_t;
+
+typedef int16_t gpio_port_t;
+#define LNG_GPIO_PORT sizeof(gpio_port_t) 
 
 /**
  * List of all system messages
  */
 typedef union _gpio_frame {
     gpio_set_t set;
+    gpio_port_t port;
 } gpio_frame_u;
     
 //Number association for standard messages
-#define GPIO_SET            0
-#define GPIO_READ_ALL       1
-#define GPIO_WRITE_ALL      2
-#define GPIO_READ           3
-#define GPIO_WRITE          4
+#define GPIO_SET       0
+#define GPIO_ALL       1
+#define GPIO           2
 
     
 #ifdef	__cplusplus
