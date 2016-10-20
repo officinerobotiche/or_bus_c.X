064e070bf13c81f72544e923a62958911115c683
Raffaello Bonghi <raffaello@rnext.it>
2016-10-20 16:27:32 +0200

    Minor fixes safety frame

--- includes/packet/frame_motor.h
+++ includes/packet/frame_motor.h
@@ -169,14 +169,16 @@ typedef struct __attribute__ ((__packed__)) _motor_parameter {
 
 /**
  * Message to launch the safety stop motor
- * - [#] Warning value max
+ * - [#]  Warning value max
  * - [ms] Timeout to start safety motor stop
  * - [ms] Auto recovery after safety mode if 0 the auto recovery is disabled
+ * - [#]  Step decrease reference
  */
 typedef struct __attribute__ ((__packed__)) _motor_safety {
-    motor_control_t warning_zone;
-    uint16_t timeout;
-    uint16_t  recovery_time;
+    motor_control_t critical_zone;
+    uint32_t timeout;
+    uint32_t recovery_time;
+    uint32_t step;
 } motor_safety_t;
 #define LNG_MOTOR_SAFETY sizeof(motor_safety_t)
 /**
